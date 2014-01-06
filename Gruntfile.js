21901941c7a5c84a41fa5c70d97a9ef7fd02fc81
Tommi Pääkkö <tommi.paakko@symbio.com>
2014-01-06 15:09:47 +0200

    A lot of changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,42 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
 
         sass : {
             dist : {
+                options : {
+                    style : 'compressed',
+                    banner : '/********************\\\n* <%= pkg.name %>\n* Build date: <%= grunt.template.today("yyyy-mm-dd") %>\n\\********************/\n',
+                },
+                files : {
+                    'build/css/ncffi.css' : 'src/_scss/ncffi.scss'
+                }
+            },
+            dev : {
+                options : {
+                    style : 'nested',
+                    sourcemap : 'true'
+                },
                 files : {
                     'build/css/ncffi.css' : 'src/_scss/ncffi.scss'
                 }
             }
+        },
+
+        watch : {
+            sass : {
+                files : 'src/_scss/**/*.scss',
+                tasks : ['sass:dev']
+            }
         }
     });
 
-    // 
     grunt.loadNpmTasks('grunt-contrib-sass');
-    //
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 
     // Default task
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass:dist']);
 
 };
